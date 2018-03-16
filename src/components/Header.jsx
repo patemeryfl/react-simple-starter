2012909d203c37b1c8cfb51fcd167b938439b399
Amanda Norton <amandanorton@Amandas-MacBook-Pro-4.local>
2018-03-16 00:29:25 -0400

    Updated styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,25 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import icons from '../assets/svgs';
+import Container from './Container';
 
 const Header = () => (
   <Fragment>
     <header className="header">
-      <nav>
-        <Link to="/" href="/">Home</Link>
-        <Link to="/about" href="/about">About</Link>
-        <Link to="/blog" href="/blog">Blog</Link>
-      </nav>
+      <Container>
+        <div className="brand">
+          <a href="https://www.reactjs.org">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path d={icons.social.rocket} />
+            </svg>
+          </a>
+        </div>
+        <nav>
+          <Link to="/" href="/">Home</Link>
+          <Link to="/about" href="/about">About</Link>
+          <Link to="/blog" href="/blog">Blog</Link>
+        </nav>
+      </Container>
     </header>
   </Fragment>
 );
